3601b8a5b836474ee7b4103807001f83f6cb46a4
Andrew Wheeler <genusis@spirean.com>
2020-08-30 01:48:41 -0400

    Made changes to how interest is set for read. Fixed read to actully disable upon a read

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,42 @@
 #![crate_type = "staticlib"]
 
-use mio::Poll;
 use libc::*;
-use std::ptr;
+use mio::Poll;
 use std::boxed::Box;
 use std::ffi::CStr;
 use std::os::raw::c_char;
+use std::ptr;
 
-mod server;
 mod client;
+mod server;
 use client::ClientState;
-use server::{Server, rust_poll_events};
+use server::{rust_poll_events, Server};
 
 #[no_mangle]
-pub extern "C" fn init_socket(cpoll: *mut Poll, ipaddress: *const c_char, client_max: u64,
-    recv: extern "C" fn(u64, *const c_char, u64) -> c_int,
-    accept: extern "C" fn(u64, *const c_char) -> c_int,
-    disconnect: extern "C" fn(u64) -> c_int) -> *mut Server {
+pub extern "C" fn init_socket(
+  cpoll: *mut Poll,
+  ipaddress: *const c_char,
+  client_max: u64,
+  recv: extern "C" fn(u64, *const c_char, u64) -> c_int,
+  accept: extern "C" fn(u64, *const c_char) -> c_int,
+  disconnect: extern "C" fn(u64) -> c_int,
+) -> *mut Server {
+  if cpoll.is_null() {
+    return ptr::null_mut();
+  }
 
-    if cpoll.is_null() {
-      return ptr::null_mut();
-    }
+  let c_str = unsafe { CStr::from_ptr(ipaddress) };
+  let f_str = match c_str.to_str() {
+    Ok(n) => n,
+    Err(_) => return ptr::null_mut(),
+  };
 
-    let c_str = unsafe { CStr::from_ptr(ipaddress) };
-    let f_str = match c_str.to_str() {
-        Ok(n) => n,
-        Err(_) => return ptr::null_mut(),
-    };
+  let server = match Server::new(cpoll, f_str, client_max as usize, recv, accept, disconnect) {
+    Ok(n) => n,
+    Err(_) => return ptr::null_mut(),
+  };
 
-    let server = match Server::new(cpoll, f_str, client_max as usize, recv, accept, disconnect) {
-        Ok(n) => n,
-        Err(_) => return ptr::null_mut(),
-    };
-
-    Box::into_raw(Box::new( server ))
+  Box::into_raw(Box::new(server))
 }
 
 #[no_mangle]
@@ -43,29 +46,29 @@ pub extern "C" fn unload_socket(csocket: *mut Server) {
       return;
     }
 
-        Box::from_raw(csocket);
-    }
+    Box::from_raw(csocket);
+  }
 }
 
 #[no_mangle]
 pub extern "C" fn init_poll() -> *mut Poll {
-    let poll = match Poll::new() {
-        Ok(n) => n,
-        Err(_) => return ptr::null_mut(),
-    };
+  let poll = match Poll::new() {
+    Ok(n) => n,
+    Err(_) => return ptr::null_mut(),
+  };
 
-    Box::into_raw(Box::new( poll ))
+  Box::into_raw(Box::new(poll))
 }
 
 #[no_mangle]
 pub extern "C" fn unload_poll(cpoll: *mut Poll) {
   unsafe {
-      if cpoll.is_null() {
-        return;
-      }
-
-      Box::from_raw(cpoll);
+    if cpoll.is_null() {
+      return;
     }
+
+    Box::from_raw(cpoll);
+  }
 }
 
 #[no_mangle]
@@ -75,9 +78,9 @@ pub extern "C" fn poll_events(cpoll: *mut Poll, cserver: *mut Server) -> c_int {
   }
 
   match rust_poll_events(cpoll, cserver) {
-        Ok(()) => 0,
-        Err(_) => -1,
-    }
+    Ok(()) => 0,
+    Err(_) => -1,
+  }
 }
 
 unsafe fn from_buf_raw<T>(ptr: *const T, size: usize) -> Vec<T> {
@@ -88,23 +91,27 @@ unsafe fn from_buf_raw<T>(ptr: *const T, size: usize) -> Vec<T> {
 }
 
 #[no_mangle]
-pub extern "C" fn socket_send(cserver: *mut Server, index: u64, data: *const c_char, size: u64) -> c_int {
-    unsafe {
-      if cserver.is_null() || data.is_null() {
-        return -1;
-      }
-
-      let bytes = from_buf_raw(data as *const u8, size as usize);
-
-      match handle_send(&mut *cserver, index, bytes) {
-        Ok(_) => return 0,
-        Err(_) => return -1,
-      }
+pub extern "C" fn socket_send(
+  cserver: *mut Server,
+  index: u64,
+  data: *const c_char,
+  size: u64,
+) -> c_int {
+  unsafe {
+    if cserver.is_null() || data.is_null() {
+      return -1;
     }
+
+    let bytes = from_buf_raw(data as *const u8, size as usize);
+
+    match handle_send(&mut *cserver, index, bytes) {
+      Ok(_) => return 0,
+      Err(_) => return -1,
+    }
+  }
 }
 
-fn handle_send (cserver: &mut Server, index: u64, data: Vec<u8>) ->Result<(), failure::Error> {
-
+fn handle_send(cserver: &mut Server, index: u64, data: Vec<u8>) -> Result<(), failure::Error> {
   let client = &mut cserver.get_mut(mio::Token(index as usize)).unwrap();
   client.send(data);
 
@@ -112,32 +119,20 @@ fn handle_send (cserver: &mut Server, index: u64, data: Vec<u8>) ->Result<(), fa
 }
 
 #[no_mangle]
-pub extern "C" fn socket_set_interest(cpoll: *mut Poll, cserver: *mut Server, index: u64, read: bool) -> c_int {
-    unsafe {
-
-    return 0;
-
-    if cserver.is_null() || cpoll.is_null() {
+pub extern "C" fn socket_set_interest(cserver: *mut Server, index: u64, read: bool) -> c_int {
+  unsafe {
+    if cserver.is_null() {
       return -1;
     }
 
-    let poll = match cpoll.as_ref() {
-        Some(n) => n,
-        None  => return -1,
-    };
-
     let server = &mut *cserver;
 
-    let client = match server.get_mut(mio::Token(index as usize)) {
-        Some(n) => n,
-        None => return -1,
-    };
-
-    client.has_read = read;
-
-    match client.reregister(&poll) {
-        Ok(()) => 0,
-        Err(_) => -1,
+    match server.get_mut(mio::Token(index as usize)) {
+      Some(client) => {
+        client.has_read = read;
+        0
+      }
+      None => return -1,
     }
   }
 }
@@ -145,7 +140,6 @@ pub extern "C" fn socket_set_interest(cpoll: *mut Poll, cserver: *mut Server, in
 #[no_mangle]
 pub extern "C" fn socket_close(cserver: *mut Server, index: u64) -> c_int {
   unsafe {
-
     if cserver.is_null() {
       return -1;
     }
@@ -155,7 +149,7 @@ pub extern "C" fn socket_close(cserver: *mut Server, index: u64) -> c_int {
     match server.get_mut(mio::Token(index as usize)) {
       Some(a) => {
         match a.close_socket() {
-          Ok(_) => {},
+          Ok(_) => {}
           Err(_) => return -1,
         };
 
@@ -165,7 +159,7 @@ pub extern "C" fn socket_close(cserver: *mut Server, index: u64) -> c_int {
           }
           _ => {}
         }
-      },
+      }
       None => {}
     }
 
--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,10 @@
+use libc::*;
 use mio::net::TcpListener;
 use mio::{Events, Poll};
 use std::collections::{HashMap, VecDeque};
-use libc::*;
-use std::time::Duration;
 use std::ffi::CString;
 use std::io;
+use std::time::Duration;
 
 use crate::client::{Client, ClientState};
 
@@ -13,135 +13,140 @@ pub const SERVER: mio::Token = mio::Token(0);
 #[repr(C)]
 #[derive(Debug)]
 pub struct Server {
-    pub listener: TcpListener,
-    pub clients: HashMap<mio::Token, Client>,
-    pub tokens: VecDeque<mio::Token>,
-    pub recv: unsafe extern "C" fn(u64, *const c_char, u64) -> c_int,
-    pub acpt: unsafe extern "C" fn(u64, *const c_char) -> c_int,
-    pub disconnect: unsafe extern "C" fn(u64) -> c_int
+  pub listener: TcpListener,
+  pub clients: HashMap<mio::Token, Client>,
+  pub tokens: VecDeque<mio::Token>,
+  pub recv: unsafe extern "C" fn(u64, *const c_char, u64) -> c_int,
+  pub acpt: unsafe extern "C" fn(u64, *const c_char) -> c_int,
+  pub disconnect: unsafe extern "C" fn(u64) -> c_int,
 }
 
 impl Server {
-    pub fn new(poll: *mut Poll, addr: &str, max: usize,
-        recv: unsafe extern "C"  fn(u64, *const c_char, u64) -> c_int,
-        acpt: unsafe extern "C"  fn(u64, *const c_char) -> c_int,
-        disconnect: unsafe extern "C"  fn(u64) -> c_int
-    ) -> Result<Server, failure::Error> {
-        /* Create a bag of unique tokens. */
-        let mut tokens = VecDeque::new();
+  pub fn new(
+    poll: *mut Poll,
+    addr: &str,
+    max: usize,
+    recv: unsafe extern "C" fn(u64, *const c_char, u64) -> c_int,
+    acpt: unsafe extern "C" fn(u64, *const c_char) -> c_int,
+    disconnect: unsafe extern "C" fn(u64) -> c_int,
+  ) -> Result<Server, failure::Error> {
+    /* Create a bag of unique tokens. */
+    let mut tokens = VecDeque::new();
 
-        for i in 1..=max {
-            tokens.push_back(mio::Token(i));
-        }
+    for i in 1..=max {
+      tokens.push_back(mio::Token(i));
+    }
 
-        /* Set up the TCP listener. */
-        let addr = addr.parse()?;
-        let mut listener = TcpListener::bind(addr)?;
+    /* Set up the TCP listener. */
+    let addr = addr.parse()?;
+    let mut listener = TcpListener::bind(addr)?;
+
+    unsafe {
+      poll
+        .as_ref()
+        .unwrap()
+        .registry()
+        .register(&mut listener, SERVER, mio::Interest::READABLE)?;
+    }
+
+    Ok(Server {
+      listener: listener,
+      clients: HashMap::new(),
+      tokens: tokens,
+      recv,
+      acpt,
+      disconnect,
+    })
+  }
+
+  pub fn accept(&mut self, poll: &Poll) -> Result<(), failure::Error> {
+    /* Wait for a new connection to accept and try to grab a token from the bag. */
+    loop {
+      let (stream, addr) = match self.listener.accept() {
+        Ok((stream, addr)) => (stream, addr),
+        Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => break,
+        Err(_e) => return Err(failure::err_msg("Failed to get accept.")),
+      };
+
+      let token = self.tokens.pop_front();
+
+      if let Some(token) = token {
+        /* We got a unique token, now let's register the new connection. */
+        let mut client = Client::new(stream, token, self.recv, self.acpt, self.disconnect);
+        client.register(poll)?;
+        self.clients.insert(token, client);
+        let cstr = match CString::new(addr.to_string()) {
+          Ok(s) => s,
+          Err(_) => return Err(failure::err_msg("Failed to get string.")),
+        };
+        let raw = cstr.into_raw();
 
         unsafe {
-          poll.as_ref().unwrap().registry()
-              .register(&mut listener, SERVER, mio::Interest::READABLE)?;
-        }
-
-        Ok(Server {
-            listener: listener,
-            clients: HashMap::new(),
-            tokens: tokens,
-            recv,
-            acpt,
-            disconnect
-        })
-    }
-
-    pub fn accept(&mut self, poll: &Poll) -> Result<(), failure::Error> {
-        /* Wait for a new connection to accept and try to grab a token from the bag. */
-        loop {
-          let (stream, addr) = match self.listener.accept() {
-            Ok((stream, addr)) => (stream, addr),
-            Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => break,
-            Err(_e) => return Err(failure::err_msg("Failed to get accept.")),
-          };
-
-          let token = self.tokens.pop_front();
-
-          if let Some(token) = token {
-              /* We got a unique token, now let's register the new connection. */
-              let mut client = Client::new(stream, token, self.recv, self.acpt, self.disconnect);
-              client.register(poll)?;
-              self.clients.insert(token, client);
-              let cstr = match CString::new(addr.to_string()) {
-                Ok(s) => s,
-                Err(_) => return Err(failure::err_msg("Failed to get string.")),
-              };
-              let raw = cstr.into_raw();
-
-              unsafe {
-                  if (self.acpt)(token.0 as u64, raw) < 0 {
-                      return Err(failure::err_msg("Failed to accept user."));
-                  }
-                  let _cstr = CString::from_raw(raw);
-              }
-
-          } else {
-              drop(stream);
+          if (self.acpt)(token.0 as u64, raw) < 0 {
+            let _cstr = CString::from_raw(raw);
+            return Err(failure::err_msg("Failed to accept user."));
           }
-      }
-        Ok(())
-    }
-
-    pub fn get_mut(&mut self, token: mio::Token) -> Option<&mut Client> {
-        /* Look up the connection for the given token. */
-        self.clients.get_mut(&token)
-    }
-
-    pub fn get(&mut self, token: mio::Token) -> Option<&Client> {
-      /* Look up the connection for the given token. */
-      self.clients.get(&token)
-    }
-
-    pub fn remove(&mut self, token: mio::Token) {
-        /* If the token is valid, let's remove the connection and add the token back to the bag. */
-        if self.clients.contains_key(&token) {
-            self.clients.remove(&token);
-            self.tokens.push_front(token);
+          let _cstr = CString::from_raw(raw);
         }
+      } else {
+        drop(stream);
+      }
     }
+    Ok(())
+  }
+
+  pub fn get_mut(&mut self, token: mio::Token) -> Option<&mut Client> {
+    /* Look up the connection for the given token. */
+    self.clients.get_mut(&token)
+  }
+
+  pub fn get(&mut self, token: mio::Token) -> Option<&Client> {
+    /* Look up the connection for the given token. */
+    self.clients.get(&token)
+  }
+
+  pub fn remove(&mut self, token: mio::Token) {
+    /* If the token is valid, let's remove the connection and add the token back to the bag. */
+    if self.clients.contains_key(&token) {
+      self.clients.remove(&token);
+      self.tokens.push_front(token);
+    }
+  }
 }
 
 pub fn rust_poll_events(cpoll: *mut Poll, cserver: *mut Server) -> Result<(), failure::Error> {
-
-    unsafe {
+  unsafe {
     let poll = &mut *cpoll;
     let server = &mut *cserver;
 
-    let mut events = Events::with_capacity(256);
+    let mut events = Events::with_capacity(1024);
 
     poll.poll(&mut events, Some(Duration::from_millis(100)))?;
 
-      for event in events.iter() {
-        match event.token() {
-            SERVER => {server.accept(&poll)?;
-              poll.registry()
-              .reregister(&mut server.listener, SERVER, mio::Interest::READABLE)?;
-            },
-            token => {
-                match server.get_mut(token) {
-                  Some(a) => {
-                    a.process(&poll, &event)?;
-
-                    match a.state {
-                      ClientState::Closed => {
-                        server.remove(token);
-                      }
-                      _ => {}
-                    }
-                  },
-                  None => {}
-                }
-            }
+    for event in events.iter() {
+      match event.token() {
+        SERVER => {
+          server.accept(&poll)?;
+          poll
+            .registry()
+            .reregister(&mut server.listener, SERVER, mio::Interest::READABLE)?;
         }
+        token => match server.get_mut(token) {
+          Some(a) => {
+            a.process(&poll, &event)?;
+
+            match a.state {
+              ClientState::Closed => {
+                server.remove(token);
+              }
+              _ => {}
+            }
+          }
+          None => {}
+        },
       }
+    }
   }
 
-    Ok(())
+  Ok(())
 }
